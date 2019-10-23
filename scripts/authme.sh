e5936464ef0da318be0c6170173f319aaace00c7
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2019-10-23 09:49:08 -0700

    fix endpoint

--- scripts/authme.sh
+++ scripts/authme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-KEPTN_ENDPOINT=https://control.keptn.$(kubectl get cm -n keptn keptn-domain -oyaml | yq - r data.app_domain)
+KEPTN_ENDPOINT=https://api.keptn.$(kubectl get cm -n keptn keptn-domain -oyaml | yq - r data.app_domain)
 KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -o=yaml | yq - r data.keptn-api-token | base64 --decode)
 
 echo "keptn endpoint: $KEPTN_ENDPOINT"
